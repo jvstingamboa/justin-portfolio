283b45abcf356a68927fe4c6bd22bf96766e96d7
jvstingamboa <jvstingamboa@gmail.com>
2023-12-14 20:13:58 -0500

    finally done :)

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,11 +5,12 @@ import ReactIcon from '../assets/react.png'
 import Node from '../assets/node.png'
 import Mongo from '../assets/mongo.png'
 import CSS from '../assets/css.png';
+import TypeScript from '../assets/ts_logo.png'
 
 
 const Skills = () => {
   return (
-    <div name ='skills' className='bg-[#0a192f] text-gray-300'>
+    <div name ='skills' className='bg-[#0a192f] text-gray-300  h-screen'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300 '>Experience</p>
@@ -25,17 +26,17 @@ const Skills = () => {
                     <img className='w-20 mx-auto py-2' src={CSS} alt="CSS icon" />
                     <p className='my-4'>CSS</p>
                 </div><div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto py-2' src={JavaScript} alt="Javascript icon" />
-                    <p className='my-4'>Javascript</p>
+                    <img className='w-20 mx-auto py-2' src={TypeScript} alt="Javascript icon" />
+                    <p className='my-4'>TypeScript</p>
                 </div><div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto py-2' src={ReactIcon} alt="React icon" />
                     <p className='my-4'>React</p>
                 </div><div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto py-2' src={HTML} alt="HTML icon" />
-                    <p className='my-4'>HTML</p>
+                    <img className='w-20 mx-auto py-2' src={Mongo} alt="HTML icon" />
+                    <p className='my-4'>MongoDB</p>
                 </div><div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto py-2' src={HTML} alt="HTML icon" />
-                    <p className='my-4'>HTML</p>
+                    <img className='w-20 mx-auto py-2' src={Node} alt="HTML icon" />
+                    <p className='my-4'>Node</p>
                 </div>
             </div>
             
