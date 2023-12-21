a64c42536e47cb597b0da46656952e06226cd42c
jvstingamboa <jvstingamboa@gmail.com>
2023-12-21 13:22:31 -0500

    took out div to move nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = () => {
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
 
         <div>
-            <img src={logo} alt='Logo Image' style={{width: "50px"}}></img>
+           
         </div>
 
 
